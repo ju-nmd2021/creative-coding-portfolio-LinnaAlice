090fea52d1d1f7002552e4b988c2007add01c062
Linna <93605506+LinnaAlice@users.noreply.github.com>
2023-09-07 14:51:37 +0200

    Added tunnels.js

---
+++ experiments/tunnels.js
@@ -0,0 +1,39 @@
+// The following two lines with the idea of making an array of set colors was adapted from https://www.seekingoutside.com/using-generative-art-to-inform-hard-edge-abstract-paintings-with-p5-js-code-snippet/ Accessed: 2023-09-01
+let colorsFill = [[109, 192, 209, 100], [184, 109, 209, 100], [204, 106, 176, 100]];
+let colorsStroke = [[82, 40, 70], [81, 48, 99], [49, 95, 102]];
+
+// I followed the tutorials on perlin noise by Daniel Shiffman uploaded on his YouTube channel playlist: https://www.youtube.com/playlist?list=PLRqwX-V7Uu6ZV4yEcW3uDwOgGXKUUsPOM
+    // However, I made my own changes to create my own artwork
+
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+}
+
+function draw() {
+    background(207, 171, 203);
+    fill(random(colorsFill));
+    stroke(random(colorsStroke));
+
+    beginShape();
+    for (let xoff = 0; xoff <= innerWidth; xoff += 100) {
+        for (let x = 0; x <= innerWidth + 30; x++) {
+            let y = map(noise(xoff), 0, 1, 0, innerHeight);
+            ellipse(x, y, noise(xoff) * 40);
+
+            xoff += 0.006;
+        }
+    }
+    endShape();
+
+    beginShape();
+    for (let xoff2 = 0; xoff2 <= innerHeight; xoff2 += 300) {
+        for (let y = 0; y <= innerHeight + 30; y++) {
+            let x = map(noise(xoff2), 0, 1, 0, innerWidth);
+            ellipse(x, y, noise(xoff2) * 40);
+
+            xoff2 += 0.006;
+        }
+    }
+    endShape();
+    noLoop();
+}
--- experiments/walkingcircles.js
+++ experiments/walkingcircles.js
@@ -1,4 +1,7 @@
 // The following line with the idea of making an array of set colors was adapted from https://www.seekingoutside.com/using-generative-art-to-inform-hard-edge-abstract-paintings-with-p5-js-code-snippet/ Accessed: 2023-09-01
+// I took inspiration from Daniel Shiffman and his videos about perlin noise on his YouTube channel https://www.youtube.com/@TheCodingTrain
+    // Specifically the use of xoff and yoff, however, I did come up with my own solution using the concept of perlin noise to create this random walker
+
 let colors = [[86, 161, 191, 100], [112, 102, 204, 100], [170, 87, 189, 100]];
 
 let x;
@@ -7,7 +10,7 @@ let xoff = 0;
 let yoff = 0;
 
 function setup() {
-    createCanvas(width, height);
+    createCanvas(innerWidth, innerHeight);
     x = 200;
     y = 200;
     background(255);
@@ -17,7 +20,7 @@ function draw() {
     stroke(255, 70);
     fill(random(colors));
 
-    x += noise(xoff) * random(-30, 30);
+    x += noise(xoff) * random(-30, 32);
     y += noise(yoff) * random(-30, 30);
 
     ellipse(x, y, noise(xoff) * random(30, 70));
