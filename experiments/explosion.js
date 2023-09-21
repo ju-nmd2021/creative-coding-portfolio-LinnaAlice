dbc4db72bc2ac3e7a182a98a56dd14243be78021
Linna <93605506+LinnaAlice@users.noreply.github.com>
2023-09-21 09:46:28 +0200

    Update explosion.js

--- experiments/explosion.js
+++ experiments/explosion.js
@@ -1,4 +1,4 @@
-// The lines 19, 28, 29, 31-35 were adapted from https://www.youtube.com/watch?v=jupjuq9Jl-M&list=PLRqwX-V7Uu6ZV4yEcW3uDwOgGXKUUsPOM&index=12&ab_channel=TheCodingTrain Accessed: 2023-09-08
+// The lines 19, 28, 29, 31-34 were adapted from https://www.youtube.com/watch?v=jupjuq9Jl-M&list=PLRqwX-V7Uu6ZV4yEcW3uDwOgGXKUUsPOM&index=12&ab_channel=TheCodingTrain Accessed: 2023-09-08
 // I followed the referenced tutorial (above) to understand and use the p5.Vector.random() function to create my own generative art
 // The following line with the idea of making an array of set colors was adapted from https://www.seekingoutside.com/using-generative-art-to-inform-hard-edge-abstract-paintings-with-p5-js-code-snippet/ Accessed: 2023-09-01
 let colors = [
